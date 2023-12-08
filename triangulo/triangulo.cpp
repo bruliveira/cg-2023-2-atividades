c311b9e937f895f075bb27c0b126576c097f9273
bruliveira <brunaflavia910@gmail.com>
2023-12-07 21:49:04 -0300

    :hammer: Adjust Triângulo

--- triangulo/triangulo.cpp
+++ triangulo/triangulo.cpp
@@ -1,36 +1,39 @@
 #include <windows.h>
 #include <GL/glut.h>
 
-int init(void){
-    glClearColor(0.0,0.0,0.0,0.0); // Cor de fundo preta
+int init(void)
+{
+    glClearColor(0.0, 0.0, 0.0, 0.0); // Cor de fundo preta
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(-10.0,10.0,-10.0,12.0);
+    gluOrtho2D(-10.0, 10.0, -10.0, 12.0);
 }
 
-void display(void){
+void display(void)
+{
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BITS);
 
     glBegin(GL_TRIANGLES);
-        glColor3f(0.0,1.0,0.0); //Verde
-        glVertex3f(-6, -6, 0.0);
+    glColor3f(0.0, 1.0, 0.0); // Verde
+    glVertex3f(-6, -6, 0.0);
 
-        glColor3f(0.0,0.0,1.0); //Azul
-        glVertex3f(0, 10, 0.0);
+    glColor3f(0.0, 0.0, 1.0); // Azul
+    glVertex3f(0, 10, 0.0);
 
-        glColor3f(1.0,0.0,0.0); //Red
-        glVertex3f(6, -6, 0.0);
+    glColor3f(1.0, 0.0, 0.0); // Red
+    glVertex3f(6, -6, 0.0);
     glEnd();
     glutSwapBuffers();
 }
 
-int main(int argc, char**  argv){
+int main(int argc, char **argv)
+{
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowPosition(50,50);
-    glutInitWindowSize(400,300);
-    glutCreateWindow("Quest„o 01 - Tri‚ngulo");
+    glutInitWindowPosition(50, 50);
+    glutInitWindowSize(400, 300);
+    glutCreateWindow("Quest√£o 1 - Tri√¢ngulo");
 
     init();
     glutDisplayFunc(display);
